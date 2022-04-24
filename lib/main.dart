1fa7bc08b1d6998d6cfcad41156551f0e23d17d4
Nathan Kim <nathan1206@gmail.com>
2022-04-24 01:26:25 -0400

    Revert "map_screen into database"

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,4 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
-import 'package:fryo/src/screens/MainInfoScreen.dart';
-import 'package:fryo/src/screens/MapScreen.dart';
-import 'package:fryo/src/screens/InfoScreen.dart';
-import 'package:fryo/src/services/ApplicationBloc.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:provider/provider.dart';
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
-import 'package:fryo/src/services/DialysisInfo.dart';
-import 'package:fryo/src/services/DialysisInfoService.dart';
-import 'package:fryo/src/shared/colors.dart';
-import 'package:fryo/src/shared/fryo_icons.dart';
-import 'package:fryo/src/shared/styles.dart';
-import 'package:url_launcher/url_launcher.dart';
-import 'package:fryo/src/shared/globals.dart' as globals;
 import 'package:fryo/src/models/user.dart';
 import 'package:fryo/src/screens/MapScreen.dart';
 import 'package:fryo/src/screens/InfoScreen.dart';
@@ -34,81 +17,9 @@ void main() async {
 }
 
 class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (context) => ApplicationBloc(),
-      child: MaterialApp(
-          title: 'DialysisNow',
-          theme: ThemeData(
-            primarySwatch: Colors.green,
-          ),
-          home: MyStatefulWidget(),//, // just for testing
-        // routes: <String, WidgetBuilder> {
-        //   '/signup': (BuildContext context) =>  SignUpPage(),
-        //   '/signin': (BuildContext context) =>  SignInPage(),
-        //   '/dashboard': (BuildContext context) => Dashboard(),
-        //   '/productPage': (BuildContext context) => ProductPage(),
-        // },
-      )
-    );
-  }
-}
-
-class MyStatefulWidget extends StatefulWidget {
-  const MyStatefulWidget({Key key}) : super(key: key);
-
-  @override
-  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
-}
-
-class _MyStatefulWidgetState extends State<MyStatefulWidget> {
-  final mainInfo = new MainInfoScreen();
-  int _selectedIndex = 1;
-  static List<Widget> _widgetOptions = globals.widgetOptions;
-
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('DialysisNow'),
-      ),
-      body: IndexedStack(
-        index: _selectedIndex,
-        children: _widgetOptions,
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-              icon: Icon(Fryo.search),
-              title: Text(
-                'Search',
-                style: tabLinkStyle,
-              )),
-          BottomNavigationBarItem(
-              icon: Icon(Fryo.star),
-              title: Text(
-                'Main Center',
-                style: tabLinkStyle,
-              )),
-          BottomNavigationBarItem(
-              icon: Icon(Fryo.alarm),
-              title: Text(
-                'Resources',
-                style: tabLinkStyle,
-              )),
-        ],
-        currentIndex: _selectedIndex,
-        type: BottomNavigationBarType.fixed,
-        fixedColor: Colors.green[600],
-        onTap: _onItemTapped,
-      ),
     return StreamProvider<User?>.value(
       initialData: null,
       value: AuthService().user,
--- lib/src/services/DialysisInfo.dart
+++ lib/src/services/DialysisInfo.dart
@@ -1,44 +1,33 @@
-import 'dart:developer';
-
 /*
 Format dialysis information from http get request.
  */
 class DialysisInfo {
 
-  final String placeID;
   final String name;
   final String address;
   final String phoneNum;
   final bool status;
   final List<dynamic> openingHours;
   final String imageRef;
-  final double lat;
-  final double lng;
 
 
   DialysisInfo({
-    this.placeID,
     this.name,
     this.address,
     this.phoneNum,
     this.status,
     this.openingHours,
     this.imageRef,
-    this.lat,
-    this.lng
   });
 
   factory DialysisInfo.fromJson(Map<String, dynamic> json) {
     return DialysisInfo(
-      placeID: json['result']['place_id'].toString(),
       name: json['result']['name'].toString(),
       address: json['result']['formatted_address'].toString(),
       phoneNum: json['result']['formatted_phone_number'].toString(),
       status: json['result']['opening_hours']['open_now'],
       openingHours: json['result']['opening_hours']['weekday_text'],
-      imageRef: json['result']['photos'][0]['photo_reference'].toString(),
-      lat: json['result']['geometry']['location']['lat'],
-      lng: json['result']['geometry']['location']['lng']
+      imageRef: json['result']['photos'][0]['photo_reference'].toString()
     );
   }
 
