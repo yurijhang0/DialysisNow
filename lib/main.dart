26331f617bcf72f5c51621dc4ec0780cd1b455c7
nathan-1206 <nathan1206@gmail.com>
2022-04-23 23:02:16 -0400

    fixing merge errors

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,7 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
-import 'package:fryo/src/screens/MainInfoScreen.dart';
 import 'package:fryo/src/models/user.dart';
 import 'package:fryo/src/screens/MapScreen.dart';
 import 'package:fryo/src/screens/InfoScreen.dart';
-import 'package:fryo/src/services/ApplicationBloc.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:provider/provider.dart';
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
-import 'package:fryo/src/services/DialysisInfo.dart';
-import 'package:fryo/src/services/DialysisInfoService.dart';
-import 'package:fryo/src/shared/colors.dart';
-import 'package:fryo/src/shared/fryo_icons.dart';
-import 'package:fryo/src/shared/styles.dart';
-import 'package:url_launcher/url_launcher.dart';
-import 'package:fryo/src/shared/globals.dart' as globals;
-
 import 'package:firebase_core/firebase_core.dart';
 import 'package:fryo/src/screens/wrapper.dart';
 import 'package:fryo/src/services/auth.dart';
@@ -27,104 +11,32 @@ import 'firebase_options.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
+      options: DefaultFirebaseOptions.currentPlatform,
   );
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-    create: (context) => ApplicationBloc(),
-    child: MaterialApp(
-    title: 'DialysisNow',
-    theme: ThemeData(
-    primarySwatch: Colors.green,
-    ),
-    home: MyStatefulWidget(),//, // just for testing
-    // routes: <String, WidgetBuilder> {
-    //   '/signup': (BuildContext context) =>  SignUpPage(),
-    //   '/signin': (BuildContext context) =>  SignInPage(),
-    //   '/dashboard': (BuildContext context) => Dashboard(),
-    //   '/productPage': (BuildContext context) => ProductPage(),
-    // },
-    )
-    );
-  }
-}
-
-class MyStatefulWidget extends StatefulWidget {
-  const MyStatefulWidget({Key key}) : super(key: key);
-
-  @override
-  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
-}
-
-class _MyStatefulWidgetState extends State<MyStatefulWidget> {
-  final mainInfo = new MainInfoScreen();
-  int _selectedIndex = 1;
-  static List<Widget> _widgetOptions = globals.widgetOptions;
-
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: const Text('DialysisNow'),
-        ),
-        body: IndexedStack(
-          index: _selectedIndex,
-          children: _widgetOptions,
-        ),
-        bottomNavigationBar: BottomNavigationBar(
-          items: <BottomNavigationBarItem>[
-            BottomNavigationBarItem(
-                icon: Icon(Fryo.search),
-                title: Text(
-                  'Search',
-                  style: tabLinkStyle,
-                )),
-            BottomNavigationBarItem(
-                icon: Icon(Fryo.star),
-                title: Text(
-                  'Main Center',
-                  style: tabLinkStyle,
-                )),
-            BottomNavigationBarItem(
-                icon: Icon(Fryo.alarm),
-                title: Text(
-                  'Resources',
-                  style: tabLinkStyle,
-                )),
-          ],
-          currentIndex: _selectedIndex,
-          type: BottomNavigationBarType.fixed,
-          fixedColor: Colors.green[600],
-          onTap: _onItemTapped,
-        ),
     return StreamProvider<User?>.value(
-    initialData: null,
-    value: AuthService().user,
-    child: MaterialApp(
-    home: Wrapper(),
-    )
-    title: 'Fryo',
-    theme: ThemeData(
-    primarySwatch: Colors.green,
-    ),
-    //, // just for testing
-    // routes: <String, WidgetBuilder> {
-    //   '/signup': (BuildContext context) =>  SignUpPage(),
-    //   '/signin': (BuildContext context) =>  SignInPage(),
-    //   '/dashboard': (BuildContext context) => Dashboard(),
-    //   '/productPage': (BuildContext context) => ProductPage(),
-    // },
+      initialData: null,
+      value: AuthService().user,
+      child: MaterialApp(
+        home: Wrapper(),
+      )
+      title: 'Fryo',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+     //, // just for testing
+      // routes: <String, WidgetBuilder> {
+      //   '/signup': (BuildContext context) =>  SignUpPage(),
+      //   '/signin': (BuildContext context) =>  SignInPage(),
+      //   '/dashboard': (BuildContext context) => Dashboard(),
+      //   '/productPage': (BuildContext context) => ProductPage(),
+      // },
     );
-    }
+  }
 }
