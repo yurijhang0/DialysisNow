e09cff3586db8910a2bed0c950f00765eacaae19
nathan-1206 <nathan1206@gmail.com>
2022-04-23 23:00:53 -0400

    fixed more merge errors

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:fryo/src/screens/MainInfoScreen.dart';
 import 'package:fryo/src/models/user.dart';
 import 'package:fryo/src/screens/MapScreen.dart';
 import 'package:fryo/src/screens/InfoScreen.dart';
@@ -24,32 +27,104 @@ import 'firebase_options.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
-      options: DefaultFirebaseOptions.currentPlatform,
+    options: DefaultFirebaseOptions.currentPlatform,
   );
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return StreamProvider<User?>.value(
-      initialData: null,
-      value: AuthService().user,
-      child: MaterialApp(
-        home: Wrapper(),
-      )
-      title: 'Fryo',
-      theme: ThemeData(
-        primarySwatch: Colors.green,
-      ),
-     //, // just for testing
-      // routes: <String, WidgetBuilder> {
-      //   '/signup': (BuildContext context) =>  SignUpPage(),
-      //   '/signin': (BuildContext context) =>  SignInPage(),
-      //   '/dashboard': (BuildContext context) => Dashboard(),
-      //   '/productPage': (BuildContext context) => ProductPage(),
-      // },
+    return ChangeNotifierProvider(
+    create: (context) => ApplicationBloc(),
+    child: MaterialApp(
+    title: 'DialysisNow',
+    theme: ThemeData(
+    primarySwatch: Colors.green,
+    ),
+    home: MyStatefulWidget(),//, // just for testing
+    // routes: <String, WidgetBuilder> {
+    //   '/signup': (BuildContext context) =>  SignUpPage(),
+    //   '/signin': (BuildContext context) =>  SignInPage(),
+    //   '/dashboard': (BuildContext context) => Dashboard(),
+    //   '/productPage': (BuildContext context) => ProductPage(),
+    // },
+    )
     );
   }
+}
+
+class MyStatefulWidget extends StatefulWidget {
+  const MyStatefulWidget({Key key}) : super(key: key);
+
+  @override
+  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
+}
+
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+  final mainInfo = new MainInfoScreen();
+  int _selectedIndex = 1;
+  static List<Widget> _widgetOptions = globals.widgetOptions;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('DialysisNow'),
+        ),
+        body: IndexedStack(
+          index: _selectedIndex,
+          children: _widgetOptions,
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          items: <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+                icon: Icon(Fryo.search),
+                title: Text(
+                  'Search',
+                  style: tabLinkStyle,
+                )),
+            BottomNavigationBarItem(
+                icon: Icon(Fryo.star),
+                title: Text(
+                  'Main Center',
+                  style: tabLinkStyle,
+                )),
+            BottomNavigationBarItem(
+                icon: Icon(Fryo.alarm),
+                title: Text(
+                  'Resources',
+                  style: tabLinkStyle,
+                )),
+          ],
+          currentIndex: _selectedIndex,
+          type: BottomNavigationBarType.fixed,
+          fixedColor: Colors.green[600],
+          onTap: _onItemTapped,
+        ),
+    return StreamProvider<User?>.value(
+    initialData: null,
+    value: AuthService().user,
+    child: MaterialApp(
+    home: Wrapper(),
+    )
+    title: 'Fryo',
+    theme: ThemeData(
+    primarySwatch: Colors.green,
+    ),
+    //, // just for testing
+    // routes: <String, WidgetBuilder> {
+    //   '/signup': (BuildContext context) =>  SignUpPage(),
+    //   '/signin': (BuildContext context) =>  SignInPage(),
+    //   '/dashboard': (BuildContext context) => Dashboard(),
+    //   '/productPage': (BuildContext context) => ProductPage(),
+    // },
+    );
+    }
 }
